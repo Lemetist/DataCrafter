96323082339a0d62b4d8ec7033d1648a4a9c52e5
Lemetist <Lemetist@gmail.com>
2024-10-22 18:32:35 +0300

    docs for filter_excel

--- filter_excel.py
+++ filter_excel.py
@@ -3,76 +3,115 @@ import json
 import math
 
 
-def split_list(input_list, chunk_size=6):
-    chunks = [input_list[i:i + chunk_size] for i in range(0, len(input_list), chunk_size)]
-    chunks = {i + 1: {j + 1: chunks[i][j] for j in range(len(chunks[i]))} for i in range(min(len(chunks), math.ceil(len(input_list) / chunk_size)))}
+def split_list(input_list, chunk_size=6) :
+    """
+    Функция разбивает список на части указанного размера и записывает результат в текстовый файл.
+
+    Аргументы:
+    - input_list (list): Исходный список для разделения.
+    - chunk_size (int, по умолчанию 6): Размер каждой части списка.
+
+    Возвращает:
+    - chunks (dict): Словарь, где ключ — номер части, а значение — вложенный словарь с порядковыми номерами и элементами части.
+    """
+    chunks = [input_list[i :i + chunk_size] for i in range(0, len(input_list), chunk_size)]
+    chunks = {i + 1 : {j + 1 : chunks[i][j] for j in range(len(chunks[i]))} for i in
+              range(min(len(chunks), math.ceil(len(input_list) / chunk_size)))}
 
     # Записываем в txt файл построчно
-    with open('split_sclud_list.txt', 'w', encoding='utf-8') as file:
+    with open('split_sclud_list.txt', 'w', encoding='utf-8') as file :
         file.write(f"{chunks}\n")
 
     return chunks
 
-def filter_excel_group(file_path, sheet_name='ОСНОВНОЕ'):
+
+def filter_excel_group(file_path, sheet_name='ОСНОВНОЕ') :
+    """
+    Функция фильтрует данные Excel по группам, отбирая столбцы с определенными предметами.
+
+    Аргументы:
+    - file_path (str): Путь к файлу Excel.
+    - sheet_name (str, по умолчанию 'ОСНОВНОЕ'): Название листа в файле.
+
+    Возвращает:
+    - group_result (dict): Словарь, где ключ — это название группы, а значение — список значений столбца.
+    """
     df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl', skiprows=3, nrows=36)
     subject1 = 'МДК.07.01 Управление и автоматизация баз данных\nДавыдова Л.Б.'
     subject2 = 'МДК.11.01 Технология разработки и защиты баз данных\nДавыдова Л.Б.'
     group_result = {}
-    for column in df.columns:
-        if subject1 in df[column].values or subject2 in df[column].values:
+    for column in df.columns :
+        if subject1 in df[column].values or subject2 in df[column].values :
             group_name = df[column].to_list()[0]
-            group_result[group_name] = df[column].to_list()[1:]
+            group_result[group_name] = df[column].to_list()[1 :]
     return group_result
 
-def filter_excel(file_path, sheet_name='ОСНОВНОЕ'):
-    # Загружаем данные из Excel
-    df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl', skiprows=5, nrows=36)
 
-    # Искомые предметы
+def filter_excel(file_path, sheet_name='ОСНОВНОЕ') :
+    """
+    Функция загружает данные из Excel и фильтрует столбцы с указанными предметами, затем разбивает данные с помощью split_list().
+
+    Аргументы:
+    - file_path (str): Путь к файлу Excel.
+    - sheet_name (str, по умолчанию 'ОСНОВНОЕ'): Название листа в файле.
+
+    Возвращает:
+    - result (dict): Словарь, где ключ — название группы, а значение — результат разбивки данных на части.
+    """
+    df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl', skiprows=5, nrows=36)
     subject1 = 'МДК.07.01 Управление и автоматизация баз данных\nДавыдова Л.Б.'
     subject2 = 'МДК.11.01 Технология разработки и защиты баз данных\nДавыдова Л.Б.'
     flag = 0
-    # Словарь для результата
     result = {}
 
     # Получаем информацию о группах
     group_data = filter_excel_group(file_path, sheet_name)
 
-    # Проходим по каждому столбцу в df
-    for column in df.columns:
-        if subject1 in df[column].values or subject2 in df[column].values:
-
-            # Извлекаем список значений из столбца (начиная со второго элемента)
-            sclud_list = df[column].to_list()[1:]
-
-            # Разбиваем список на части
+    # Проходим по каждому столбцу
+    for column in df.columns :
+        if subject1 in df[column].values or subject2 in df[column].values :
+            sclud_list = df[column].to_list()[1 :]
             split_sclud_list = split_list(sclud_list)
 
-            # Обновляем результат для текущего столбца
             if flag < len(group_data) :
                 group_name = list(group_data.keys())[flag]
                 result[group_name] = split_sclud_list
                 flag += 1
     return result
 
-def filter_subjects(data, subjects):
+
+def filter_subjects(data, subjects) :
+    """
+    Фильтрует данные, оставляя только указанные предметы.
+
+    Аргументы:
+    - data (dict): Исходные данные для фильтрации.
+    - subjects (list): Список предметов для фильтрации.
+
+    Возвращает:
+    - filtered_data (dict): Словарь с отфильтрованными данными по предметам.
+    """
     filtered_data = {}
-    for column, rows in data.items():
-        for row, cells in rows.items():
-            for cell, value in cells.items():
-                if value in subjects:
-                    if column not in filtered_data:
+    for column, rows in data.items() :
+        for row, cells in rows.items() :
+            for cell, value in cells.items() :
+                if value in subjects :
+                    if column not in filtered_data :
                         filtered_data[column] = {}
-                    if row not in filtered_data[column]:
+                    if row not in filtered_data[column] :
                         filtered_data[column][row] = {}
                     filtered_data[column][row][cell] = value
     return filtered_data
 
+
+# Пример использования
 result = filter_excel('download_file.xlsx')
 print(result)
-print(filter_subjects(result, ['МДК.07.01 Управление и автоматизация баз данных\nДавыдова Л.Б.', 'МДК.11.01 Технология разработки и защиты баз данных\nДавыдова Л.Б.']))
+
+filtered_result = filter_subjects(result, ['МДК.07.01 Управление и автоматизация баз данных\nДавыдова Л.Б.',
+                                           'МДК.11.01 Технология разработки и защиты баз данных\nДавыдова Л.Б.'])
+print(filtered_result)
 
 # Сохранение результата в JSON файл
-with open('result.json', 'w', encoding='utf-8') as json_file:
+with open('result.json', 'w', encoding='utf-8') as json_file :
     json.dump(result, json_file, ensure_ascii=False, indent=4)
-
