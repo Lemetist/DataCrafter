9160f6707a9b0760e7798d3796d4a06ae0f0e98e
SuusMaan <ilyaserzhantov@yandex.ru>
2024-10-27 22:32:50 +0300

    123

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import sys
 from datetime import datetime
 import locale
 import json
+import pandas as pd
+import requests
+import os
+import math
 
 class SimpleApp(QWidget):
     def __init__(self):
@@ -12,8 +16,11 @@ class SimpleApp(QWidget):
         locale.setlocale(locale.LC_TIME, 'Russian_Russia.1251')
         self.light_theme = True 
         self.font_size = 14 
-        self.load_settings()  # Загрузка настроек 
-        self.initUI()  # Инициализация интерфейса 
+        self.load_settings()  
+        self.initUI()  
+
+        # Загрузка файла при запуске приложения
+        self.download_and_process_file()
 
     def initUI(self):
         self.setWindowTitle('SusMan -- Lemetist')
@@ -31,7 +38,6 @@ class SimpleApp(QWidget):
         self.text_output.setReadOnly(True)
         layout.addWidget(self.text_output)
 
-        # Инициализация рамки для кнопок 
         self.button_frame = QFrame(self)
         self.button_frame.setStyleSheet("QFrame { border: 2px solid #A77BCA; border-radius: 10px; padding: 10px; }")
 
@@ -65,18 +71,108 @@ class SimpleApp(QWidget):
 
         layout.addWidget(self.button_frame)
 
-        # Статусная строка 
         self.status_bar = QStatusBar(self)
         layout.addWidget(self.status_bar)
-
-        self.setLayout(layout)
 
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_date)
         self.timer.start(1000)
         self.update_date()
 
-        self.set_theme()  # Применяем тему после инициализации интерфейса 
+        self.set_theme()  
+
+        self.setLayout(layout)
+
+    def download_file(self):
+        url = 'https://docs.google.com/spreadsheets/d/1S3kj0zo_QDERJu7O2QU1J4gMRx-K381m/export?format=xlsx'
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  
+
+            file_path = 'download_file.xlsx'
+            with open(file_path, 'wb') as f:
+                f.write(response.content)
+            self.display_message(f'Файл сохранен как {file_path}')
+        except requests.exceptions.RequestException as e:
+            self.display_message(f'Ошибка загрузки файла: {e}')
+
+    def wb_name(self):
+        file_path = "download_file.xlsx"
+        
+        if os.path.exists(file_path):
+            wb = pd.ExcelFile(file_path)
+            return wb.sheet_names
+        else:
+            self.display_message(f'Файл {file_path} не найден.')
+            return None
+
+    def download_and_process_file(self):
+        self.display_message("Начинаем скачивание файла...")
+        self.download_file()  # Скачиваем файл
+        sheet_names = self.wb_name()  # Получаем имена листов
+        if sheet_names:
+            self.display_message("Имена листов: " + ", ".join(sheet_names))
+            result = self.filter_excel('download_file.xlsx')
+            self.display_message(f"Результат фильтрации: {result}")
+
+            # Сохранение результата в JSON файл
+            with open('result.json', 'w', encoding='utf-8') as json_file:
+                json.dump(result, json_file, ensure_ascii=False, indent=4)
+            self.display_message("Результат сохранен в result.json")
+
+    def split_list(self, input_list, chunk_size=6):
+        chunks = [input_list[i: i + chunk_size] for i in range(0, len(input_list), chunk_size)]
+        chunks = {i + 1: {j + 1: chunks[i][j] for j in range(len(chunks[i]))} for i in range(min(len(chunks), math.ceil(len(input_list) / chunk_size)))}
+
+        with open('split_sclud_list.txt', 'w', encoding='utf-8') as file:
+            file.write(f"{chunks}\n")
+
+        return chunks
+
+    def filter_excel_group(self, file_path, sheet_name='ОСНОВНОЕ'):
+        df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl', skiprows=3, nrows=36)
+        subject1 = 'МДК.07.01 Управление и автоматизация баз данных\nДавыдова Л.Б.'
+        subject2 = 'МДК.11.01 Технология разработки и защиты баз данных\nДавыдова Л.Б.'
+        group_result = {}
+        for column in df.columns:
+            if subject1 in df[column].values or subject2 in df[column].values:
+                group_name = df[column].to_list()[0]
+                group_result[group_name] = df[column].to_list()[1:]
+        return group_result
+
+    def filter_excel(self, file_path, sheet_name='ОСНОВНОЕ'):
+        df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl', skiprows=5, nrows=36)
+        subject1 = 'МДК.07.01 Управление и автоматизация баз данных\nДавыдова Л.Б.'
+        subject2 = 'МДК.11.01 Технология разработки и защиты баз данных\nДавыдова Л.Б.'
+        flag = 0
+        result = {}
+
+        group_data = self.filter_excel_group(file_path, sheet_name)
+
+        for column in df.columns:
+            if subject1 in df[column].values or subject2 in df[column].values:
+                sclud_list = df[column].to_list()[1:]
+                split_sclud_list = self.split_list(sclud_list)
+
+                if flag < len(group_data):
+                    group_name = list(group_data.keys())[flag]
+                    result[group_name] = split_sclud_list
+                    flag += 1
+        return result
+
+    def filter_subjects(self, data, subjects):
+        filtered_data = {}
+        for column, rows in data.items():
+            for row, cells in rows.items():
+                for cell, value in cells.items():
+                    if value in subjects:
+                        if column not in filtered_data:
+                            filtered_data[column] = {}
+                        if row not in filtered_data[column]:
+                            filtered_data[column][row] = {}
+                        filtered_data[column][row][cell] = value
+        return filtered_data
+
     def add_button_animation(self, button):
         animation = QPropertyAnimation(button, b"geometry")
         original_geometry = button.geometry()
@@ -91,8 +187,88 @@ class SimpleApp(QWidget):
             self.status_bar.showMessage("Текст очищен.")
         elif text == 'Настройки':
             self.open_settings_dialog()
+        elif text == 'Добавить':
+            # Load data from result.json
+            try:
+                with open('result.json', 'r', encoding='utf-8') as json_file:
+                    data = json.load(json_file)
+            except FileNotFoundError:
+                self.display_message("Файл result.json не найден.")
+                return
+            except json.JSONDecodeError:
+                self.display_message("Ошибка при чтении файла result.json.")
+                return
+
+            # Define subjects for filtering
+            subjects = [
+                'МДК.07.01 Управление и автоматизация баз данных\nДавыдова Л.Б.',
+                'МДК.11.01 Технология разработки и защиты баз данных\nДавыдова Л.Б.'
+            ]
+
+            # Filter the subjects
+            filtered_subjects = self.filter_subjects(data, subjects)
+
+            # Save filtered subjects to filter_subjects.json
+            with open('filter_subjects.json', 'w', encoding='utf-8') as json_file:
+                json.dump(filtered_subjects, json_file, ensure_ascii=False, indent=4)
+
+            # Clear the text output before displaying
+            self.text_output.clear()
+
+            # Mapping of numbers to days
+            day_mapping = {
+                "1": "Понедельник",
+                "2": "Вторник",
+                "3": "Среда",
+                "4": "Четверг",
+                "5": "Пятница",
+                "6": "Суббота",
+                "7": "Воскресенье"
+            }
+
+            # Display the filtered subjects with modified values
+            for key, value in filtered_subjects.items():
+                formatted_output = f"{key}:\n"  # Изменено: добавлен перевод строки
+                for day_number, classes in value.items():
+                    # Проверяем, является ли classes строкой, если да, то преобразуем
+                    if isinstance(classes, str):
+                        try:
+                            classes_dict = eval(classes)  # Преобразуем строку в словарь
+                        except Exception as e:
+                            self.display_message(f"Ошибка при обработке классов: {e}")
+                            continue
+                    elif isinstance(classes, dict):
+                        classes_dict = classes  # Если это уже словарь, просто присваиваем
+
+                    # Добавляем день недели в вывод без номера
+                    formatted_output += f"{day_mapping[day_number]}:\n"  # Название дня
+
+                    # Добавляем номера классов перед классами
+                    for class_key, class_value in classes_dict.items():
+                        formatted_output += f"  {class_key}. {class_value}\n"  # Номер класса и класс
+                    formatted_output += "\n"  # Добавляем пустую строку после каждого дня для разделения
+
+                self.text_output.append(formatted_output.strip())  # Убираем лишний пробел в конце
+
+            self.status_bar.showMessage("Фильтрованные данные добавлены в текстовое поле и сохранены в filter_subjects.json.")
         else:
             self.display_message(f"Вы нажали кнопку: {text}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def display_message(self, message):
         self.text_output.append(message)
@@ -117,7 +293,7 @@ class SimpleApp(QWidget):
                 padding: 10px;
                 font-size: 14px;
             }
-        """)
+            """)
         else:
             self.setStyleSheet("""
             QWidget {
@@ -133,9 +309,8 @@ class SimpleApp(QWidget):
                 padding: 10px;
                 font-size: 14px;
             }
-        """)
+            """)
 
-        # Обновляем стиль текстового поля с новым размером шрифта 
         self.text_output.setStyleSheet(f"border: 1px solid #BBBBBB; padding: 10px; font-size: {self.font_size}px;")
 
     def update_date(self):
@@ -165,22 +340,16 @@ class SimpleApp(QWidget):
         theme_button.clicked.connect(self.toggle_theme)
         layout.addWidget(theme_button)
 
-        # Анимация изменения размера диалогового окна 
-        dialog.setGeometry(0, 0, 300, 0)  # Начальная высота 0
-        animation = QPropertyAnimation(dialog, b"geometry")
-        animation.setDuration(300)
-        animation.setStartValue(QRect(dialog.x(), dialog.y(), 300, 0))  # Начинаем с высоты 0 
-        animation.setEndValue(QRect(dialog.x(), dialog.y(), 300, 200))  # Конечная высота 200
-        animation.start()
         dialog.setLayout(layout)
         dialog.exec_()
 
     def update_font_size(self, value):
         self.font_size = value 
         self.font_size_label.setText(f"Размер шрифта: {self.font_size}px")
-        self.set_theme()  # Обновляем тему, чтобы применить новый размер шрифта 
+        self.set_theme()  
         self.text_output.setStyleSheet(f"border: 1px solid #BBBBBB; padding: 10px; font-size: {self.font_size}px;")
-        self.save_settings()  # Сохраняем настройки при изменении 
+        self.save_settings()  
+
     def save_settings(self):
         settings = {
             'light_theme': self.light_theme,
@@ -196,7 +365,7 @@ class SimpleApp(QWidget):
                 self.light_theme = settings.get('light_theme', True)
                 self.font_size = settings.get('font_size', 14)
         except FileNotFoundError:
-            pass  # Файл настроек не найден, используем значения по умолчанию
+            pass  
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
